01b67958cb28a97e444ba1ad620fcaa2d7de91be
Erik Langenborg <erik.langenborg@gmail.com>
2016-05-04 18:44:18 -0400

    Added React. Because COOL VIEW LAYER!

--- typings/references.d.ts
+++ typings/references.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="./node.d.ts" />
 /// <reference path="./babylon.2.3.d.ts" />
 /// <reference path="./ramda.d.ts" />
+/// <reference path="./react.d.ts" />
+/// <reference path="./react-dom.d.ts" />
