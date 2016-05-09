86390aa5d3f930f63aa9f0ec37548b24f5506074
Erik Langenborg <erik.langenborg@gmail.com>
2016-05-09 00:47:34 -0400

    Toying with directions and cameras

--- code/screen/race/racer.ts
+++ code/screen/race/racer.ts
@@ -13,7 +13,8 @@ export class Racer extends Player {
   private roller: BABYLON.Mesh;
 
   public get Camera() { return this.camera; }
-  private camera: BABYLON.ArcFollowCamera;
+  // private camera: BABYLON.ArcFollowCamera;
+  private camera: BABYLON.FreeCamera;
   private baseMesh: BABYLON.AbstractMesh;
 
   private linearHelper: BABYLON.LinesMesh;
@@ -30,17 +31,18 @@ export class Racer extends Player {
     scene: BABYLON.Scene,
     spawn: BABYLON.Vector3
   ) => {
+    const color = BABYLON.Color3.FromHexString(`#${this.Color.toString(16)}`);
     this.baseMesh = new BABYLON.AbstractMesh(`base.${this.DeviceId}`, scene);
 
     this.linearHelper = BABYLON.Mesh.CreateLines("lines", [
       new BABYLON.Vector3(0, 0, 0),
       new BABYLON.Vector3(0, 0, -10)
     ], scene);
-    this.linearHelper.color = new BABYLON.Color3(0.8, 0, 0);
+    this.linearHelper.color = color;
     this.linearHelper.parent = this.baseMesh;
 
     const sphereMat = new BABYLON.StandardMaterial(`spheremat.${this.DeviceId}`, scene);
-    sphereMat.diffuseColor = BABYLON.Color3.FromHexString(`#${this.Color.toString(16)}`);
+    sphereMat.diffuseColor = color;
     sphereMat.wireframe = true;
     this.roller = BABYLON.Mesh.CreateSphere(`roller.${this.DeviceId}`, 6, 2.5, scene);
     this.roller.material = sphereMat;
@@ -48,13 +50,19 @@ export class Racer extends Player {
     this.roller.setPhysicsState(BABYLON.PhysicsEngine.SphereImpostor,
       { mass: 5, friction: 1.5, restitution: 0.1 });
 
-    this.camera = new BABYLON.ArcFollowCamera('camera2', 0, Math.PI / 6, 25, this.roller, scene);
+    // this.camera = new BABYLON.ArcFollowCamera(`camera.${this.DeviceId}`, Math.PI, Math.PI / 6, 25, this.roller, scene);
+    this.camera = new BABYLON.FreeCamera(`camera.${this.DeviceId}`, this.baseMesh.position.add(new BABYLON.Vector3(-20, 8, 0)), scene);
+    this.camera.lockedTarget = this.baseMesh;
+    // this.camera.parent = this.baseMesh;
 
     scene.activeCameras.push(this.camera);
 
 
     window.addEventListener("keyup", (evt) => {
       console.log('Kick');
+      const linear: BABYLON.Vector3 = (<any>this.roller.getPhysicsImpostor()).getLinearVelocity();
+      console.log(linear.length(), linear.toString());
+
       this.roller.applyImpulse(new BABYLON.Vector3(10.5, 0, 0), this.roller.getAbsolutePosition());
     });
 
@@ -62,14 +70,21 @@ export class Racer extends Player {
 
   }
 
-  public onEveryFrame = (delta: number) => {
-    this.baseMesh.position = this.roller.position;
-    this.Camera.alpha += Math.PI / 1024;
+  public onEveryFrame = (millisecondsSinceLastFrame: number) => {
+    // console.log(millisecondsSinceLastFrame);
     const linear: BABYLON.Vector3 = (<any>this.roller.getPhysicsImpostor()).getLinearVelocity();
-    console.log(linear.toString());
+    const movement = linear.lengthSquared() < 0.05 ? new BABYLON.Vector3(20, 0, 0) : linear;
+
+    const flippedMovement = movement.multiplyByFloats(1, 0, 1).normalize().negate().scale(15).add(new BABYLON.Vector3(0, 6, 0));
+    this.baseMesh.position = this.roller.position;
+    // this.camera.position = this.baseMesh.position.add(new BABYLON.Vector3(-20, 8, 0));
+    this.camera.position = this.baseMesh.position.add(flippedMovement);
+    // this.Camera.alpha += Math.PI / 1024;
+    // this.Camera.alpha = 1;
+    // console.log(linear.toString());
 
     let scale = linear.lengthSquared() / 50;
-    this.linearHelper.scaling = new BABYLON.Vector3(scale,scale,scale);
+    this.linearHelper.scaling = new BABYLON.Vector3(scale, scale, scale);
     this.linearHelper.lookAt(this.baseMesh.position.add(linear), 0, 0, 0);
   }
 
