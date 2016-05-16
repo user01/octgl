751254a0060eb540298455f590be9693a9919eca
Erik Langenborg <erik.langenborg@gmail.com>
2016-05-16 01:39:09 -0400

    Removed old comments

--- code/screen/race/racer.hud.tsx
+++ code/screen/race/racer.hud.tsx
@@ -95,17 +95,4 @@ class RacerHUD extends React.Component<IAppProps, IAppState> {
 
 }
 
-// <div className={`player-box ${this.props.class}`} style={style}>
-// <h1 style={style} className="screen-header">
-//   Device: {this.props.racer.DeviceId}
-// </h1>
-// <h3 style={style} className="screen-header">
-//   Velocity: {Math.round(this.props.racer.LinearVelocity * 100) / 100}
-// </h3>
-// <h3 style={style} className="screen-header">
-//   Z Velocity: {this.props.racer.zLinear}
-// </h3>
-// <h3 style={style} className="screen-header">
-//   Lap: {this.props.racer.Lap} / {Math.floor(this.props.racer.PercentDoneTrack * 100) } / Next {this.props.racer.CurrentTrackIndex}
-// </h3>
 export default RacerHUD;
