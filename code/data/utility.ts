bbca31b44c994cba0f0e6b357732e4510c3b8081
Erik Langenborg <erik.langenborg@gmail.com>
2016-05-22 18:50:38 -0400

    Added skeleton for rate limiter

--- code/data/utility.ts
+++ code/data/utility.ts
@@ -1,5 +1,6 @@
 
 /// <reference path="../../typings/references.d.ts" />
+import * as R from 'ramda';
 
 /** A Utility Class
 */
@@ -28,6 +29,56 @@ export class Utility {
   public static RandomItem<T>(items: T[]) {
     return items[Math.floor(Math.random() * items.length)];
   }
+}
+
+export class MessageLimiter {
+  private pastMessages: number[] = []
+  private periodicUpdateId: any = -1;
+  private static PERIODIC_UPDATE_MS = 200;
+
+  private get messagesInLastSecond() { return this.pastMessages.length; }
+
+  constructor(
+    private limitPerSecond: number,
+    private realMessage: (device_id: number, message: any) => void
+  ) {
+  }
+
+  public Message = (device_id: number, message: any) => {
+    this.purgeOldMessages();
+    this.enableSendingMessages();
+    if (this.messagesInLastSecond < this.limitPerSecond - 1) {
+      this.send(device_id, message);
+    } else {
+      //queue up data
+    }
+  }
+
+  private send = (device_id: number, message: any) => {
+    this.pastMessages.push(this.Now);
+    this.realMessage(device_id, message);
+  }
+
+  private enableSendingMessages = () => {
+    if (this.periodicUpdateId != -1) return;
+    this.periodicUpdateId = setInterval(this.perodicUpdate, MessageLimiter.PERIODIC_UPDATE_MS);
+  }
+
+  public StopSendingMessages = () => {
+    window.clearTimeout(this.periodicUpdateId);
+    this.periodicUpdateId = -1;
+  }
+
+  private perodicUpdate = () => {
+
+  }
+
+  private purgeOldMessages = () => {
+    const oneSecondAgo = this.Now - 1000;
+    this.pastMessages = this.pastMessages.filter(ms => ms > oneSecondAgo);
+  }
+
+  private get Now() { return +new Date(); }
 }
 
 export default Utility;
